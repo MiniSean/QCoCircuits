7bd4d38b3c7cba9e2c3cf28596ba067aa4d3a4cb
SeanvdMeer <18538762+MiniSean@users.noreply.github.com>
2024-02-05 23:05:13 +0100

    Added generic code layer for defining arbitrary gate-sequences, for any type of parity check code.

---
+++ src/qce_circuit/connectivity/intrf_connectivity_gate_sequence.py
@@ -0,0 +1,118 @@
+# -------------------------------------------
+# Module containing interface for inter-qubit interaction gate connectivity.
+# -------------------------------------------
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import List, Union, TypeVar, Generic
+from enum import Enum, unique, auto
+from qce_circuit.utilities.custom_exceptions import InterfaceMethodException
+from qce_circuit.utilities.array_manipulation import unique_in_order
+from qce_circuit.connectivity.intrf_channel_identifier import IEdgeID, IQubitID
+
+
+@unique
+class OperationType(Enum):
+    IDLE = auto()
+    PARK = auto()
+    GATE = auto()  # Refers to 2-qubit gate
+
+
+TIdentifier = TypeVar('TIdentifier', bound=Union[IQubitID, IEdgeID])
+
+
+@dataclass(frozen=True)
+class Operation(Generic[TIdentifier]):
+    identifier: TIdentifier
+    type: OperationType
+
+    # region Class Methods
+    def contains(self, element: TIdentifier) -> bool:
+        if self.identifier == element:
+            return True
+        if isinstance(self.identifier, IEdgeID) and self.identifier.contains(element):
+            return True
+        return False
+
+    @classmethod
+    def type_idle(cls, quibt_id: IQubitID) -> 'Operation':
+        return Operation[IQubitID](
+            identifier=quibt_id,
+            type=OperationType.IDLE,
+        )
+
+    @classmethod
+    def type_park(cls, quibt_id: IQubitID) -> 'Operation':
+        return Operation[IQubitID](
+            identifier=quibt_id,
+            type=OperationType.PARK,
+        )
+
+    @classmethod
+    def type_gate(cls, edge_id: IEdgeID) -> 'Operation':
+        return Operation[IEdgeID](
+            identifier=edge_id,
+            type=OperationType.GATE,
+        )
+    # endregion
+
+
+@dataclass(frozen=True)
+class GateSequenceLayer:
+    """
+    Data class, containing directional gates played during 'flux-dance' layer.
+    """
+    _park_operations: List[Operation[IQubitID]]
+    """Parking operations, identified by IQubitID."""
+    _gate_operations: List[Operation[IEdgeID]]
+    """Gate operations, identified by non-directional IEdgeID."""
+
+    # region Class Properties
+    @property
+    def qubit_ids(self) -> List[IQubitID]:
+        """:return: All qubit-ID's, both gate and parking candidates."""
+        park_qubits: List[IQubitID] = [operation.identifier for operation in self._park_operations]
+        gate_qubits: List[IQubitID] = [identifier for operation in self._gate_operations for identifier in operation.identifier.qubit_ids]
+        return unique_in_order(park_qubits + gate_qubits)
+
+    @property
+    def edge_ids(self) -> List[IEdgeID]:
+        """:return: Array-like of directional edge identifiers, specific for this flux dance."""
+        gate_edges: List[IEdgeID] = [operation.identifier for operation in self._gate_operations]
+        return unique_in_order(gate_edges)
+    # endregion
+
+    # region Class Methods
+    def contains(self, element: Union[IQubitID, IEdgeID]) -> bool:
+        """:return: Boolean, whether element is part of flux-dance layer or not."""
+        if element in self.qubit_ids:
+            return True
+        if element in self.edge_ids:
+            return True
+        return False
+    # endregion
+
+
+class IGateSequenceLayer(ABC):
+    """
+    Interface class, describing methods for obtaining flux-dance information.
+    """
+
+    # region Interface Properties
+    @property
+    @abstractmethod
+    def gate_sequence_count(self) -> int:
+        """:return: Number of gate-sequences in layer."""
+        raise InterfaceMethodException
+    # endregion
+
+    # region Interface Methods
+    @abstractmethod
+    def get_gate_sequence_at_index(self, index: int) -> GateSequenceLayer:
+        """:return: Gate-sequence object based on round index."""
+        raise InterfaceMethodException
+
+    @abstractmethod
+    def get_gate_sequence_from_element(self, element: IEdgeID) -> GateSequenceLayer:
+        """:return: Gate-sequence layer of which edge element is part of."""
+        raise InterfaceMethodException
+    # endregion
---
+++ src/qce_circuit/library/repetition_code_connectivity.py
@@ -0,0 +1,138 @@
+# -------------------------------------------
+# Module containing repetition-code connectivity.
+# -------------------------------------------
+from qce_circuit.utilities.singleton_base import SingletonABCMeta
+from qce_circuit.connectivity.intrf_channel_identifier import (
+    QubitIDObj,
+    EdgeIDObj,
+)
+from qce_circuit.connectivity.connectivity_surface_code import (
+    ParityGroup,
+    ParityType,
+)
+from qce_circuit.connectivity.generic_gate_sequence import (
+    IGenericSurfaceCodeLayer,
+    GenericSurfaceCode,
+)
+from qce_circuit.connectivity.intrf_connectivity_gate_sequence import (
+    GateSequenceLayer,
+    Operation,
+)
+
+
+class Repetition9Code(GenericSurfaceCode, IGenericSurfaceCodeLayer, metaclass=SingletonABCMeta):
+
+    # region Class Constructor
+    def __init__(self):
+        super().__init__(
+            gate_sequences=[
+                GateSequenceLayer(
+                    _park_operations=[
+                        Operation.type_park(QubitIDObj('Z3')),
+                        Operation.type_park(QubitIDObj('D8')),
+                        Operation.type_park(QubitIDObj('Z4')),
+                        Operation.type_park(QubitIDObj('D9')),
+                        Operation.type_park(QubitIDObj('X2')),
+                        Operation.type_park(QubitIDObj('D2')),
+                        Operation.type_park(QubitIDObj('D3')),
+                    ],
+                    _gate_operations=[
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X1'), QubitIDObj('D1'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z1'), QubitIDObj('D4'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X3'), QubitIDObj('D7'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z2'), QubitIDObj('D6'))),
+                    ],
+                ),
+                GateSequenceLayer(
+                    _park_operations=[
+                        Operation.type_park(QubitIDObj('D7')),
+                        Operation.type_park(QubitIDObj('Z4')),
+                        Operation.type_park(QubitIDObj('D9')),
+                        Operation.type_park(QubitIDObj('X2')),
+                        Operation.type_park(QubitIDObj('D1')),
+                    ],
+                    _gate_operations=[
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X1'), QubitIDObj('D2'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z1'), QubitIDObj('D5'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X3'), QubitIDObj('D8'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z2'), QubitIDObj('D3'))),
+                    ],
+                ),
+                GateSequenceLayer(
+                    _park_operations=[
+                        Operation.type_park(QubitIDObj('X3')),
+                        Operation.type_park(QubitIDObj('D9')),
+                        Operation.type_park(QubitIDObj('Z1')),
+                        Operation.type_park(QubitIDObj('D1')),
+                        Operation.type_park(QubitIDObj('D3')),
+                    ],
+                    _gate_operations=[
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X2'), QubitIDObj('D2'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z4'), QubitIDObj('D5'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X4'), QubitIDObj('D8'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z3'), QubitIDObj('D7'))),
+                    ],
+                ),
+                GateSequenceLayer(
+                    _park_operations=[
+                        Operation.type_park(QubitIDObj('X3')),
+                        Operation.type_park(QubitIDObj('D7')),
+                        Operation.type_park(QubitIDObj('D8')),
+                        Operation.type_park(QubitIDObj('Z1')),
+                        Operation.type_park(QubitIDObj('D1')),
+                        Operation.type_park(QubitIDObj('D2')),
+                        Operation.type_park(QubitIDObj('Z2')),
+                    ],
+                    _gate_operations=[
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X2'), QubitIDObj('D3'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z4'), QubitIDObj('D6'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X4'), QubitIDObj('D9'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z3'), QubitIDObj('D4'))),
+                    ],
+                ),
+            ],
+            parity_group_z=[
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X1'),
+                    _data_qubits=[QubitIDObj('D1'), QubitIDObj('D2')]
+                ),
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X2'),
+                    _data_qubits=[QubitIDObj('D2'), QubitIDObj('D3')]
+                ),
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X3'),
+                    _data_qubits=[QubitIDObj('D7'), QubitIDObj('D8')]
+                ),
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X4'),
+                    _data_qubits=[QubitIDObj('D8'), QubitIDObj('D9')]
+                ),
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_Z,
+                    _ancilla_qubit=QubitIDObj('Z1'),
+                    _data_qubits=[QubitIDObj('D4'), QubitIDObj('D5')]
+                ),
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_Z,
+                    _ancilla_qubit=QubitIDObj('Z2'),
+                    _data_qubits=[QubitIDObj('D3'), QubitIDObj('D6')]
+                ),
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_Z,
+                    _ancilla_qubit=QubitIDObj('Z3'),
+                    _data_qubits=[QubitIDObj('D4'), QubitIDObj('D7')]
+                ),
+                ParityGroup(
+                    _parity_type=ParityType.STABILIZER_Z,
+                    _ancilla_qubit=QubitIDObj('Z4'),
+                    _data_qubits=[QubitIDObj('D5'), QubitIDObj('D6')]
+                ),
+            ],
+            parity_group_x=[],
+        )
+    # endregion
