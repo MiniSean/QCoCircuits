2695d1f093e71b6a6cc74420da7a70247b3266c9
SeanvdMeer <18538762+MiniSean@users.noreply.github.com>
2024-02-11 10:15:37 +0100

    Added apply noise method to addon_stim module __init__.py.

--- src/qce_circuit/addon_stim/__init__.py
+++ src/qce_circuit/addon_stim/__init__.py
@@ -3,8 +3,14 @@ from .factory_manager import (
     StimFactoryManager,
     to_stim,
 )
+from .noise_factory_manager import (
+    StimNoiseDresserFactoryManager,
+    apply_noise,
+)
 
 __all__ = [
     "StimFactoryManager",
     "to_stim",
+    "StimNoiseDresserFactoryManager",
+    "apply_noise",
 ]
