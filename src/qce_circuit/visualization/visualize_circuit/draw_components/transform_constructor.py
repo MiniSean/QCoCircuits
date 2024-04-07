33b593009e002c19643e9c87ee6280758aeb215e
SeanvdMeer <18538762+MiniSean@users.noreply.github.com>
2024-04-07 13:55:33 +0200

    Added first version of multi-draw components offset for visual clarity.

--- src/qce_circuit/visualization/visualize_circuit/draw_components/transform_constructor.py
+++ src/qce_circuit/visualization/visualize_circuit/draw_components/transform_constructor.py
@@ -35,3 +35,36 @@ class TransformConstructor(ITransformConstructor):
         """:return: Rectilinear transform height based on channel identifier."""
         return self.channel_height
     # endregion
+
+
+@dataclass(frozen=True)
+class OffsetTransformConstructor(ITransformConstructor):
+    """
+    Behaviour class, implementing ITransformConstructor interface,
+    with an additional off-center x-offset (for artistic purposes).
+    """
+    default_transform: ITransformConstructor
+    pivot_offset_scalar_x: float
+    """Scalar that offsets pivot in X-direction. x0 + xoff * time_component.duration."""
+
+    # region Interface Methods
+    def identifier_to_pivot(self, identifier: ChannelIdentifier, time_component: IDurationComponent) -> Vec2D:
+        """:return: Pivot based on channel identifier and duration component."""
+        default_pivot = self.default_transform.identifier_to_pivot(
+            identifier=identifier,
+            time_component=time_component,
+        )
+        return Vec2D(
+            x=default_pivot.x + self.pivot_offset_scalar_x * time_component.duration,
+            y=default_pivot.y
+        )
+
+    def identifier_to_width(self, time_component: IDurationComponent) -> float:
+        """:return: Rectilinear transform height based on duration component."""
+        return self.default_transform.identifier_to_width(time_component=time_component)
+
+    def identifier_to_height(self, identifier: ChannelIdentifier) -> float:
+        """:return: Rectilinear transform height based on channel identifier."""
+        return self.default_transform.identifier_to_height(identifier=identifier)
+    # endregion
+
