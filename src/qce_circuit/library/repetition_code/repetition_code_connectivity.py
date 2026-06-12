4e09253944e49ef8858b6f88d264cec8fa2abadf
SeanvdMeer <18538762+minisean@users.noreply.github.com>
2026-06-12 20:14:02 +0200

    Added distance 9 repetition code for X basis

--- src/qce_circuit/library/repetition_code/repetition_code_connectivity.py
+++ src/qce_circuit/library/repetition_code/repetition_code_connectivity.py
@@ -375,6 +375,117 @@ class Repetition9Round2Code(GenericSurfaceCode, IGenericSurfaceCodeLayer, metacl
     # endregion
 
 
+class Repetition9Round2XCode(GenericSurfaceCode, IGenericSurfaceCodeLayer, metaclass=SingletonABCMeta):
+
+    # region Class Constructor
+    def __init__(self):
+        super().__init__(
+            gate_sequences=[
+                GateSequenceLayer(
+                    _park_operations=[
+                    ],
+                    _gate_operations=[
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X4'), QubitIDObj('D8'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X3'), QubitIDObj('D7'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z3'), QubitIDObj('D4'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z1'), QubitIDObj('D5'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z4'), QubitIDObj('D6'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z2'), QubitIDObj('D3'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X2'), QubitIDObj('D2'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X1'), QubitIDObj('D1'))),
+                    ],
+                ),
+                GateSequenceLayer(
+                    _park_operations=[
+                    ],
+                    _gate_operations=[
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X4'), QubitIDObj('D9'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X3'), QubitIDObj('D8'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z3'), QubitIDObj('D7'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z1'), QubitIDObj('D4'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z4'), QubitIDObj('D5'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('Z2'), QubitIDObj('D6'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X2'), QubitIDObj('D3'))),
+                        Operation.type_gate(EdgeIDObj(QubitIDObj('X1'), QubitIDObj('D2'))),
+                    ],
+                ),
+            ],
+            parity_group_z=[
+            ],
+            parity_group_x=[
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('Z1'),
+                    _data_qubits=[ QubitIDObj('D4'), QubitIDObj('D5')]
+                ),
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('Z2'),
+                    _data_qubits=[QubitIDObj('D3'), QubitIDObj('D6')]
+                ),
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('Z3'),
+                    _data_qubits=[QubitIDObj('D4'), QubitIDObj('D7')]
+                ),
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('Z4'),
+                    _data_qubits=[QubitIDObj('D5'), QubitIDObj('D6')]
+                ),
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X1'),
+                    _data_qubits=[ QubitIDObj('D1'), QubitIDObj('D2')]
+                ),
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X2'),
+                    _data_qubits=[QubitIDObj('D2'), QubitIDObj('D3')]
+                ),
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X3'),
+                    _data_qubits=[QubitIDObj('D7'), QubitIDObj('D8')]
+                ),
+                ParityGroup(
+                    _parity_type=StabilizerType.STABILIZER_X,
+                    _ancilla_qubit=QubitIDObj('X4'),
+                    _data_qubits=[QubitIDObj('D8'), QubitIDObj('D9')]
+                ),
+            ],
+            logical_observables=[
+                LogicalObservable(
+                    observable_basis=StabilizerType.STABILIZER_X,
+                    data_qubit_projections={
+                        QubitIDObj("D1"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D2"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D3"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D4"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D5"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D6"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D7"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D8"): StabilizerType.STABILIZER_X,
+                        QubitIDObj("D9"): StabilizerType.STABILIZER_X,
+                    },
+                    supporting_stabilizers=[
+                        QubitIDObj("Z1"),
+                        QubitIDObj("Z2"),
+                        QubitIDObj("Z3"),
+                        QubitIDObj("Z4"),
+                        QubitIDObj("X1"),
+                        QubitIDObj("X2"),
+                        QubitIDObj("X3"),
+                        QubitIDObj("X4"),
+                    ],
+                    default_projections=StabilizerType.STABILIZER_X
+                )
+            ],
+            surface_code_layer=SurfaceTuna17Layer(),
+        )
+    # endregion
+
+
 class Repetition5Round4Code(GenericSurfaceCode, IGenericSurfaceCodeLayer, metaclass=SingletonABCMeta):
     """
     Data class, containing repetition code for distance-5 as ran for the error injection measurements (XX-11-2024)
